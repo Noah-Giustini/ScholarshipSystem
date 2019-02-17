bd9e64bb0f3185c9c1931e95dc015faac19a981d
julio.agostini <julio.agostini@cs.ucalgary.ca>
2019-02-17 10:28:58 -0700

    added student class with method stubs

---
+++ Student.java
@@ -0,0 +1,32 @@
+public class Student implements User{
+
+    public Application apply() {
+        return null;
+    }
+
+    public void editApplication(Application app) {
+        
+    }
+
+    public void viewApplications() {
+   
+    }
+
+    public void withdrawApplication(Application app) {
+    
+    }
+
+    public void viewScholarships(){
+        
+    }
+    
+    public void acceptScholarship(Scholarship scholarship) {
+        
+    }
+
+    public void declineScholarship(Scholarship scholarship) {
+    
+    }
+
+
+}
