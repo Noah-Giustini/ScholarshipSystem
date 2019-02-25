ff30bcb9b5e3174267fb35f2f1cb61633c9c6381
Noah Giustini <36166847+Noah-Giustini@users.noreply.github.com>
2019-02-24 20:16:05 -0700

    Update Admin.java
    Added functionality to the createScholarship method

--- Admin.java
+++ Admin.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Admin implements User{
     public void viewApplications() {
         //to be implemented
@@ -9,7 +11,62 @@ public class Admin implements User{
         //to be implemented
     }
     public Scholarship createScholarship() {
-        //to be implemented
+        Scanner scan = new Scanner(System.in);
+        String name;
+        String dueDate;
+        double amt;
+        int recipients;
+        ArrayList<String> levels = new ArrayList<String>();
+
+        System.out.println("What is the name of the scholarship you wish to create?");
+        name = scan.nextLine();
+        Scholarship scol = new Scholarship(name);
+        int dateGood = 0;
+        while (dateGood == 0){
+            System.out.println("What will the due date of the scholarship be?");
+            dueDate = scan.nextLine();
+            if (Pattern.matches("\\d\\d/\\d\\d/\\d\\d\\d\\d", dueDate)){
+                scol.setDueDate(dueDate);
+                dateGood = 1;
+            }
+            else{
+                System.out.println("Invalid format. must follow mm/dd/yyyy");
+            }
+        }
+        int amtGood = 0;
+        while (amtGood == 0){
+            System.out.println("How much will the scholarship be worth");
+            try{
+                amt = Double.parseDouble(scan.nextLine());
+                amtGood = 1;
+            }
+            catch(NumberFormatException e){
+                System.out.println("Invalid input. Must be of type double");
+            }
+        }
+        scol.setAmount(amt);
+        int recGood = 0;
+        while (recGood == 0){
+            System.out.println("How many people can recieve this scholarship?");
+            try{
+                recipients = Integer.parseInt(scan.nextLine());
+                recGood = 1;
+            }
+            catch(NumberFormatException e){
+                System.out.println("Invalid input. Must be of type integer");
+            }
+        }
+        scol.setRecipients(recipients);
+        System.out.println("What levels is this scholarship available for?")
+        while (sc.hasNextString()) {
+            int i = scan.nextString();
+            levels.add(i);
+        }
+        scol.setLevels(levels);
+        return scol;
+
+
+
     }
     public void modRemovScholarship() {
         //to be implemented
